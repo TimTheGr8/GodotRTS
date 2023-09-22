6e0dda3bc653ebf2a6b44f25a42f44000fe68b65
TimTheGr8 <tjanssen4@gmail.com>
2023-09-22 14:36:25 -0500

    Added the ability to select and move the player. Also added the sprite flashing red when it gets attacked.

--- Scripts/Unit.gd
+++ Scripts/Unit.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+class_name Unit
 
 @export var health : int = 100
 @export var damage : int = 20
@@ -10,13 +11,24 @@ var last_attack_time : float
 
 var target : CharacterBody2D
 
-@onready var nav_agent = get_node("NavigationAgent2D")
-@onready var sprite = get_node("Sprite")
+var nav_agent : NavigationAgent2D
+var sprite : Sprite2D
 
 @export var is_player : bool
 
 func _ready():
-	print("Ready")
+	nav_agent = $NavigationAgent2D
+	sprite = $Sprite
+
+func _process(delta):
+	target_check()
+
+func _physics_process(delta):
+	if nav_agent.is_navigation_finished():
+		return
+	var direction = global_position.direction_to(nav_agent.get_next_path_position())
+	velocity = direction * move_speed
+	move_and_slide()
 
 func move_to_location(location):
 	target = null
@@ -24,6 +36,15 @@ func move_to_location(location):
 
 func set_target(new_target):
 	target = new_target
+
+func target_check():
+	if target != null:
+		var dist = global_position.distance_to(target.global_position)
+		if dist <= attack_range:
+			nav_agent.target_position = global_position
+			_try_attack_target()
+		else:
+			nav_agent.target_position = target.global_position
 
 func _try_attack_target():
 	var current_time = Time.get_unix_time_from_system()
@@ -35,4 +56,7 @@ func take_damage(damage_to_take):
 	health -= damage_to_take
 	if health <= 0:
 		queue_free()
+	sprite.modulate = Color.RED
+	await get_tree().create_timer(0.1).timeout
+	sprite.modulate = Color.WHITE
 
